8317078c5c0a9ebd6687df76b28ca0a579fadf8d
paullkw <paulzmc@gmail.com>
2026-04-19 08:51:24 +0800

    Fix login

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,18 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === '/') {
+    const token = await getToken({
+      req: request,
+      secret: process.env.NEXTAUTH_SECRET,
+    });
+
+    if (token) {
+      return NextResponse.next();
+    }
+
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
